68981a7a18a057f677537bfca33ab9cfa8d6238c
shaileshyedge <yedgeshailesh@gmail.com>
2016-05-03 03:04:57 -0700

    Fix FarmerFeed

--- public/javascripts/services/FarmerService.js
+++ public/javascripts/services/FarmerService.js
@@ -41,7 +41,7 @@ angular.module("farmerhelp").factory("FarmerService",["$window","$http","$q", fu
                 }
             }, function (error) {
                 if(error.status === 302) {
-                    $window.location.href = "http://localhost:3000/#auth/login";
+                    $window.location.href = "http://farmerhelp.mybluemix.net/#auth/login";
                 }
                 def.reject(error);
             });
